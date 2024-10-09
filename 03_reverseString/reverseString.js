895d9b41f7b071c7fb9e0d27da4d57e8e528998b
Ayman <155395474+datastatz@users.noreply.github.com>
2024-10-09 20:54:13 +0200

    Finished exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
+const reverseString = function(word) {
+
+return word.split('').reverse().join('');
 
 };
-
+console.log(reverseString('hello there')); // Outputs: 'ereht olleh'
 // Do not edit below this line
 module.exports = reverseString;
+
