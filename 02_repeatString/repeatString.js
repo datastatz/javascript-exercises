6657cbcb8c60382d935d6a086f304c5cb7466a0b
Ayman <155395474+datastatz@users.noreply.github.com>
2024-10-09 20:38:02 +0200

    Exercises done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,23 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    //Initializing the result
+    let result = ''
 
+    //If num is negative than we return an error
+    if (num < 0) {
+        return "ERROR"
+    }
+
+    //Repeat the string num number of times
+    for (i = 0 ; i < num; i++) {
+        result += string;
+    }
+
+    // Returning the final result
+    return result;
 };
+
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
